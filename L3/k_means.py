9ebb6547331da10a3b95473d5715f2554cd67e47
Łukasz Arczyński <lukaszarczynski@gmail.com>
2017-11-21 04:33:16 +0100

    Computations in K-Means can be resumed after interruption

--- L3/k_means.py
+++ L3/k_means.py
@@ -1,19 +1,20 @@
 import numpy as np
 
-
 class KMeans:
     def __init__(self, *, k):
         self.k = k
+        self.total_iterations = 0
         self.iterations = None
         self.group_centers = None
         self.groups = None
         self._data_length = None
         self._data_dimension = None
+        self.partition_changed = None
 
     def _get_nth_group(self, data, partition=None, *, n):
         if partition is None:
             partition = self.groups
-        return data[partition==n, :]
+        return data[partition == n, :]
 
     def random_centers(self, data):
         random_without_repetition = np.random.choice(np.arange(self._data_length),
@@ -36,26 +37,27 @@ class KMeans:
         group_centers = np.zeros([self.k, self._data_dimension])
         for i in range(self.k):
             group = self._get_nth_group(data, partition, n=i)
-            group_centers[i] = np.sum(group, axis=0, keepdims=True) / group.shape[0]
+            if len(group) == 0:
+                group_centers[i] = np.random.choice(np.arange(self._data_length), 1)
+            else:
+                group_centers[i] = np.sum(group, axis=0, keepdims=True) / group.shape[0]
         return group_centers
 
-    def fit(self, data):
+    def fit(self, data, *, max_iterations=300):
         self._data_length = data.shape[0]
         self._data_dimension = data.shape[1]
-        group_centers = self.random_centers(data)
-
-        partition = np.zeros(self._data_dimension)
-        partition_changed = True
+        if self.group_centers is None:
+            self.group_centers = self.random_centers(data)
+            self.groups = np.zeros(self._data_dimension)
+            self.partition_changed = True
         self.iterations = 0
-        while partition_changed:
+        while self.partition_changed and self.iterations < max_iterations:
+            new_partition = self.nearest_neighbors(data, self.group_centers)
+            self.partition_changed = not np.array_equal(self.groups, new_partition)
+            self.groups = new_partition
+            self.group_centers = self.centroids(data, self.groups)
             self.iterations += 1
-            new_partition = self.nearest_neighbors(data, group_centers)
-            partition_changed = not np.array_equal(partition, new_partition)
-            partition = new_partition
-            group_centers = self.centroids(data, partition)
-
-        self.group_centers = group_centers
-        self.groups = partition
+            self.total_iterations += 1
         return self
 
     def average_distances_to_center(self, data):
@@ -78,19 +80,21 @@ class KMeans:
         return average_distances
 
 
-
 if __name__ == "__main__":
     from matplotlib import pyplot
+
     data = np.random.uniform(-1, 1, [100, 2])
     print(data)
     pyplot.scatter(data[:, 0], data[:, 1])
     pyplot.show()
-    data_partition = KMeans(k=3).fit(data)
-    colormap = "rgb"
-    print(data_partition.iterations)
-    pyplot.scatter(data[:, 0], data[:, 1], c=[colormap[i] for i in data_partition.groups])
-    pyplot.scatter(data_partition.group_centers[:, 0],
-                   data_partition.group_centers[:, 1], c="black")
-    pyplot.show()
+    data_partition = KMeans(k=3)
+    for _ in range(10):
+        data_partition.fit(data)
+        colormap = "rgb"
+        print(data_partition.iterations)
+        pyplot.scatter(data[:, 0], data[:, 1], c=[colormap[i] for i in data_partition.groups])
+        pyplot.scatter(data_partition.group_centers[:, 0],
+                       data_partition.group_centers[:, 1], c="black")
+        pyplot.show()
     print(data_partition.average_distances_to_center(data))
     print(data_partition.center_to_center_average_distance())
